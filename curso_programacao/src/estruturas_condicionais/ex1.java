35d5accd556cfe6fa78af3a0ab713ab73564f507
bsanches <bsanches@CET-026023.marginal.cetsp.com.br>
2020-06-05 18:22:47 -0300

    Exercícios para iniciantes PARTE 2 - Estrutura Condicional

---
+++ curso_programacao/src/estruturas_condicionais/ex1.java
@@ -0,0 +1,22 @@
+package estruturas_condicionais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		//Estrutura Condicional Simples
+		Locale.setDefault(Locale.US);		
+		Scanner sc = new Scanner(System.in);
+		double nota1 = sc.nextDouble();
+		double nota2 = sc.nextDouble();
+		double notaFinal = nota1 + nota2;
+		System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
+		
+		if (notaFinal < 60.0) {
+			System.out.println("REPROVADO");
+			}	
+		sc.close();
+	}
+}
