b1f7594290b616f4e99471fe70de79c2fd922b23
bmine <bmine@BrunoMinetti>
2020-06-05 23:18:46 -0300

    Exercícios estruturas condicionais

---
+++ curso_programacao/src/estruturas_condicionais/ex3.java
@@ -0,0 +1,29 @@
+package estruturas_condicionais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex3 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if (A < B && A < C) {
+			System.out.println("MENOR = " + A);
+		}
+		else if (B < A && B < C) {
+			System.out.println("MENOR = " + B);
+		}
+		else {
+			System.out.println("MENOR = " + C);
+		}
+
+		sc.close();
+
+	}
+
+}
