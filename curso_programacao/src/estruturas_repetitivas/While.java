bed22c1e3a76aae9b49676d2e4fcf1a554299922
bsanches <bsanches@CET-026023.marginal.cetsp.com.br>
2020-06-11 18:17:52 -0300

    while

--- curso_programacao/src/estruturas_repetitivas/While.java
+++ curso_programacao/src/estruturas_repetitivas/While.java
@@ -1,13 +1,24 @@
 package estruturas_repetitivas;
 
+import java.util.Scanner;
+
 public class While {
 
 	public static void main(String[] args) {
-		int x = 10;
-		while (x > 0) {
-			x -=1;
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int soma = 0;
+		
+		while (x != 0) {
+			soma += x; 
+			x = sc.nextInt();
+			
 		}
-		System.out.println(x);
+		System.out.println(soma);
+		
+		sc.close();
 	}
 
 }
