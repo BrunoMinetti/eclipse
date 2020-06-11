063e98fca862f3a77bc70e320e8514bf677c514d
bsanches <bsanches@CET-026023.marginal.cetsp.com.br>
2020-06-11 16:13:31 -0300

    While

---
+++ curso_programacao/src/estruturas_repetitivas/While.java
@@ -0,0 +1,13 @@
+package estruturas_repetitivas;
+
+public class While {
+
+	public static void main(String[] args) {
+		int x = 10;
+		while (x > 0) {
+			x -=1;
+		}
+		System.out.println(x);
+	}
+
+}
