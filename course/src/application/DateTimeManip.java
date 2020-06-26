b67149d8f0e362939bea8704e487ac2e8dbd3626
bsanches <bsanches@CET-026023.marginal.cetsp.com.br>
2020-06-26 14:52:00 -0300

    date

--- course/src/application/DateTimeManip.java
+++ course/src/application/DateTimeManip.java
@@ -3,6 +3,7 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateTimeManip {
@@ -14,6 +15,16 @@ public class DateTimeManip {
 		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 		
 		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+
+		
+
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Months: " + month);
 		
 	}
 }
