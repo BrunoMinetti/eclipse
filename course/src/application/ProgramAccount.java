abb6a8a7fc37e31727f67b25e7d83dfea1ada974
Bruno Sanches <brunominetti@gmail.com>
2020-07-17 18:01:26 -0300

    exercício 129

--- course/src/application/ProgramAccount.java
+++ course/src/application/ProgramAccount.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -7,16 +11,34 @@ import entities.SavingsAccount;
 public class ProgramAccount {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		List<Account> list = new ArrayList<>();
 		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
+		list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.00, 400.0));
+		list.add(new SavingsAccount(1004, "Bob", 300.00, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 		
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+		double sum = 0.0;
+		for(Account acc : list) {
+			sum += acc.getBalance();
+		}
 		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for(Account acc : list) {
+			double x = acc.getBalance();
+			acc.deposit(10);
+			System.out.printf("O valor da conta %d foi alterado de $ %.2f para $ %.2f%n", acc.getNumber(), x, acc.getBalance());
+		}
+		
+		sum = 0.0;
+		for(Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
 	}
 
 }
